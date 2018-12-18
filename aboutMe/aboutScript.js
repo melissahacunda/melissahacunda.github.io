30ffd4b2a6735305441549869c7d63ec2fc4d844
MelissaHacunda <melissa.hacunda@gmail.com>
2018-12-18 00:10:35 -0500

    firstcommit

---
+++ aboutMe/aboutScript.js
@@ -0,0 +1,22 @@
+// Google Maps
+var myCenter = new google.maps.LatLng(9.9150603, 122.8321918);
+
+function initialize() {
+  var mapProp = {
+    center: myCenter,
+    zoom: 12,
+    scrollwheel: false,
+    draggable: false,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
+
+  var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+
+  var marker = new google.maps.Marker({
+    position: myCenter,
+  });
+
+  marker.setMap(map);
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
